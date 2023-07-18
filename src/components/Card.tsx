69ba0d5d93562af41838ba4edecc99dae2942ef0
kevin <kevindiegodasilvasousa@gmail.com>
2023-07-18 19:05:44 -0300

    wip: init style card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,41 +1,31 @@
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
-
-export default function BasicCard() {
-  return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-  );
+interface PropsCardImage {
+  image: string;
 }
+
+interface PropsCardText {
+  title: string;
+  subtitle: string;
+  paragraph: string;
+}
+
+export type Props = PropsCardImage | PropsCardText;
+
+export const Card = (props: Props) => {
+  if ('image' in props) {
+    // Renderizar o componente de imagem
+    return (
+      <section className="w-[650px] bg-slate-300 rounded m-5">
+        <img src={props.image} alt="" />
+      </section>
+    );
+  }
+
+  // Renderizar o componente de texto
+  return (
+    <section className="w-[650px] bg-slate-300 rounded m-5">
+      <h2 className="font-bold text-4xl text-center">{props.title}</h2>
+      <h3>{props.subtitle}</h3>
+      <p>{props.paragraph}</p>
+    </section>
+  );
+};
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,20 @@
+import { Card } from "../../components/Card"
 import { Header } from "../../components/Header"
 import { Profiles } from "../../components/Profiles"
+import andershejlsberg from "../../assets/anders-hejlsberg.png"
 
 const Home = () => {
   return (
     <>
       <Header />
-      <section>
-        <h3>Quem somos?</h3>
-        <p></p>
+      <section className="flex gap-3">
+        <Card
+          title="TypeScript: The Future of JavaScript"
+          subtitle="Who is typescript and who created it?"
+          paragraph="TypeScript is an open-source programming language developed by Microsoft and designed by Anders Hejlsberg. It extends JavaScript with features like static typing, interfaces, classes, and modules, enabling more robust and scalable code. TypeScript's static typing helps catch errors early, offers autocompletion, and enhances code quality. It compiles to JavaScript, making it compatible with any JavaScript environment.
+
+        Anders Hejlsberg, known for his work on languages like Turbo Pascal and Delphi, created TypeScript to address JavaScript development challenges. It introduces static typing for better tooling support and error detection. TypeScript is compatible with JavaScript, facilitating smooth integration into existing projects without extensive code rewriting. Its features enhance development productivity while ensuring code reliability."/>
+        <Card image={andershejlsberg} />
       </section>
       <section>
         <h3>Nossos objetivos</h3>
