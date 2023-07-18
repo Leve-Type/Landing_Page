60f0ec7914312054879379d6bdf796fdd0740968
kevin <kevindiegodasilvasousa@gmail.com>
2023-07-17 23:57:44 -0300

    feat: create basic html

---
+++ src/components/Profiles.tsx
@@ -0,0 +1,11 @@
+export const Profiles = () => {
+    return (
+        <div className="flex flex-1 gap-5">
+            <a href="https://github.com/NatanMoura085.Gsilva"><img className="w-20 rounded-full" src="https://github.com/NatanMoura085.png" alt="natan moura" /></a>
+            <a href="https://github.com/kevindsousa.Gsilva"><img className="w-20 rounded-full" src="https://github.com/kevindsousa.png" alt="kevin sousa" /></a>
+            <a href="https://github.com/Bruno-Gsilva"><img className="w-20 rounded-full" src="https://github.com/Bruno-GSilva.png" alt="bruno silva" /></a>
+            <a href="https://github.com/LucasDCoelho.Gsilva"><img className="w-20 rounded-full" src="https://github.com/LucasDCoelho.png" alt="lucas coelho" /></a>
+
+        </div>
+    )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,40 @@
-const Home = () =>{
-    return(
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    )
-  }
-  
-  export default Home
+import { Profiles } from "../../components/Profiles"
+
+const Home = () => {
+  return (
+    <>
+      <header>
+        <img className="w-20 rounded-sm" src="https://github.com/Leve-Type.png" alt="leve type logo" />
+        <h2>Leve type community</h2>
+      </header>
+      <section>
+        <h3>Quem somos?</h3>
+        <p></p>
+      </section>
+      <section>
+        <h3>Nossos objetivos</h3>
+        <p></p>
+      </section>
+      <section>
+        <h3>Colaboradores</h3>
+        <Profiles />
+      </section>
+      <footer>
+        <div className="">
+          <ul>
+            <li><a href="#">Link 1</a></li>
+            <li><a href="#">Link 2</a></li>
+            <li><a href="#">Link 3</a></li>
+            <li><a href="#">Link 4</a></li>
+          </ul>
+        </div>
+
+        <div className="">
+          <p>&copy; 2023 Levetype. Todos os direitos reservados.</p>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Home
